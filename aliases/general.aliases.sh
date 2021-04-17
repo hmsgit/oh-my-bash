02c3c6ac7d68910e1359359701152c3e6dca4193
Hossain Mahmud <hossain.mahmud@gmail.com>
2021-04-17 12:57:58 +0200

    Reordered prompt

--- aliases/general.aliases.sh
+++ aliases/general.aliases.sh
@@ -15,8 +15,8 @@
 #   1.  MAKE TERMINAL BETTER
 #   -----------------------------
 
-alias cp='cp -iv'                           # Preferred 'cp' implementation
-alias mv='mv -iv'                           # Preferred 'mv' implementation
+# alias cp='cp -iv'                           # Preferred 'cp' implementation
+# alias mv='mv -iv'                           # Preferred 'mv' implementation
 alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
 alias ll='ls -lAFh'                         # Preferred 'ls' implementation
 alias less='less -FSRXc'                    # Preferred 'less' implementation
@@ -28,4 +28,4 @@ alias show_options='shopt'                  # Show_options: display bash options
 alias fix_stty='stty sane'                  # fix_stty:     Restore terminal settings when screwed up
 alias fix_term='echo -e "\033c"'            # fix_term:     Reset the conosle.  Similar to the reset command
 alias cic='set completion-ignore-case On'   # cic:          Make tab-completion case-insensitive
-alias src='source ~/.bashrc'                # src:          Reload .bashrc file
+# alias src='source ~/.bashrc'                # src:          Reload .bashrc file
